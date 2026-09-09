e58787062f579b0872efcca82fb794aa14d0c938
LKl <plat.elena2010@ya.ru>
2026-09-09 13:20:23 +0300

    TypeScript +

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Header(): React.ReactElement {
+  return (
+    <header className="header">
+      <div className="header__logo">Мой Диплом</div>
+    </header>
+  );
+}
